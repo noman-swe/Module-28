1e63c100c84ec19e9dd8c387ff7fc5829f68efd9
noman-swe <noman.swe130@gmail.com>
2022-08-13 12:49:50 +0600

    28-6 Use event bubble to create calculator and clear

--- 28-5-Pin-Match/js/app.js
+++ 28-5-Pin-Match/js/app.js
@@ -1,16 +1,32 @@
- function getPin(){
+function getPin() {
     const pin = Math.round(Math.random() * 10000);
     const pinString = pin + '';
-    if(pinString.length == 4){
+    if (pinString.length == 4) {
         return pin;
-    } else{
+    } else {
         return getPin();
     }
-         
+
 }
 
 
-function generatePin(){
+function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumbers = document.getElementById('display-typedNum');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            typedNumbers.value = '';
+        }
+    }
+    else {
+        const previousNumber = (typedNumbers.value); //this one is string, integer + string = string;        
+        const newNumber = previousNumber + number;
+        typedNumbers.value = newNumber;
+    }
+
+});
